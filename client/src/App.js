57e89758c51b83e36e3cf8907ae5c8f761323c76
Basia <b.kumor@yahoo.com>
2020-09-10 20:16:30 +0200

    save id do ls

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Login from './components/Login';
+import useLS from './hooks/useLS';
 
 function App() {
-  const [id, setId] = useState(null);
+  const [id, setId] = useLS('id');
   return (
     <div className="App">
+      {id}
       <Login setId={setId}/>
     </div>
   );
