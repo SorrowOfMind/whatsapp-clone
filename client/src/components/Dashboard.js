27b86e7ebcfdd58885a8f4c61a4a8de88d367468
Basia <b.kumor@yahoo.com>
2020-09-17 16:24:24 +0200

    conv box

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ConversationBox from './ConversationBox';
 import Sidebar from './Sidebar';
+import {useConversations} from '../contexts/ConversationContext';
 
 const Dashboard = ({id}) => {
+    const {selectedConversation} = useConversations();
     return (
         <div className="d-flex" style={{height: '100vh'}}>
             <Sidebar id={id}/>
+            {selectedConversation && <ConversationBox />}
         </div>
     )
 }
